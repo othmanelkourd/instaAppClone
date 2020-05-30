f2ea973de3c328804edede0ad71296dae56b3a06
Othman ELKOURD <othman.elkourd@thetrainline.com>
2020-05-30 10:19:38 +0200

    initial commit

---
+++ app/src/main/java/com/othman/instaappclone/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.othman.instaappclone.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
